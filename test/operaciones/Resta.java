b51b08d4c511898d0055f98493e2c0f7a2c78a48
Franchu <franciscobaezasanchez13@gmail.com>
2026-02-09 21:43:43 +0100

    Implementacion de clase resta

---
+++ test/operaciones/Resta.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author franc
+ */
+public class Resta {
+    public int restar(int a, int b) {
+        return a - b;
+    }
+}
