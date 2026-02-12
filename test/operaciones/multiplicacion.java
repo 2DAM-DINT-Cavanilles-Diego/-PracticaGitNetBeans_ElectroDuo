38dca86f68b331e857d7d31b329cc710220ca997
Franchu <franciscobaezasanchez13@gmail.com>
2026-02-12 19:44:57 +0100

    Implementación metodo multiplicar

---
+++ test/operaciones/multiplicacion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author franc
+ */
+public class multiplicacion {
+    public int multiplicar(int a, int b){
+        return a * b;
+    }
+}
