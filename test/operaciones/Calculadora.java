97b15977b8296ef6fcfecc88fa601172283e0feb
Franchu <franciscobaezasanchez13@gmail.com>
2026-02-12 19:53:16 +0100

    clase principal calculadora

---
+++ test/operaciones/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author franc
+ */
+public class Calculadora {
+    public static void main(String[] args){
+        Suma s = new Suma();
+        Resta r = new Resta();
+        Multiplicacion m = new Multiplicacion();
+        
+        System.out.println("5 + 3 = " + s.sumar(5, 3));
+        System.out.println("5 - 3 = " + r.restar(5, 3));
+        System.out.println("5 * 3 = " + m.multiplicar(5, 3));
+    }
+}
